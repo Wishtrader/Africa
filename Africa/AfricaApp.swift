05d5d2b8c5c4f1e29e33963021ddd4d653f9589d
Andrei Kamarou <mr.akomarov@gmail.com>
2021-09-18 03:09:07 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Andrei Kamarou on 18.09.21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
